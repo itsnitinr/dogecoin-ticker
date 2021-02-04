ce3ac5cbfa77587b8c84ad896c5e05a4a20e35dd
Nitin Ranganath <nitinranganath@gmail.com>
2021-02-04 19:44:20 +0530

    Initial design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import logo from './logo.png';
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <h1>Dogecoin Ticker</h1>
+    <div className="App">
+      <img src={logo} width={150} height={150} alt="Dogecoin Logo" />
+      <h1 className="title">Live Dogecoin Price</h1>
+      <h5 className="subtitle">Dogecoin To The Moon 🚀🌕</h5>
     </div>
   );
 };
